09cafb21429fa0a964d76c65dc621e790f039f20
James Hall <rio@x5g.com>
2011-02-03 15:41:03 +0000

    Remove temp folder

--- adapters/titanium.js
+++
@@ -1,9 +0,0 @@
-
-var db = Titanium.Database.open('fishingscrapbook');
-var rows = db.execute('SELECT * FROM scrapbooks');
-while (rows.isValidRow()) {
-	var name = rows.fieldByName('NAME');
-	console.log(name);
-	rows.next();
-}
-
